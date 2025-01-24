e7f4ef08259e7845ba9dfb6b29698ca49612f535
yeissonvels <yeisson.velez@gmail.com>
2025-01-25 00:50:38 +0100

    Se añade config BBDD riodevs

---
+++ conf/config_riodevs.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'host'      => 'localhost',
+    'user'      => 'u934467215_formas',
+    'password'  => 'D3s4rr0ll0F0rm4s!',
+    'dbname'    => 'u934467215_formas',
+    'prefix'    => 'ge_',
+    'defaultTimeZone' => 'Europe/Madrid',
+);
